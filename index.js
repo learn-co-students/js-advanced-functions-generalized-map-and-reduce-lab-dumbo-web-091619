10043bda0248aa9472d2254936b1c499c84a62f7
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-11-10 10:42:47 -0500

    Done.

--- index.js
+++ index.js
@@ -18,14 +18,3 @@ const reduce = (arr, func, num = 0) => {
     }
     return result
 }
-
-// let  reduce = (arr, func, num = 0) =>{
-//     let result = (!!num) ? num : arr[0]
-//     let i = (!!num) ? 0 : 1
-  
-//     for (; i < arr.length; i++) {
-//       result = func(arr[i], result)
-//     }
-  
-//     return result;
-//   }
